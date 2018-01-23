bf0ce0ef783f811d28b5b67af909416d26ee44f1
fujimoto <fujimoto@liberteenz.jp>
2018-01-23 18:34:12 +0900

    Added a namespace

--- Assets/Editor/CIBuilder.cs
+++ Assets/Editor/CIBuilder.cs
@@ -3,46 +3,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 
-class CIBuilder{
-    static string[] scenes = FindEnabledEditorScenes();
+namespace AutoBuild {
+    class CIBuilder {
+        static string[] scenes = FindEnabledEditorScenes();
 
-    static string appname = "YourProject";
-    static string targetPath = "AutoBuild";
+        static string appname = "YourProject";
+        static string targetPath = "AutoBuild";
 
-    private static string[] FindEnabledEditorScenes() {
-        List<string> EditorScenes = new List<string>();
-        foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes) {
-            if (!scene.enabled) continue;
-            EditorScenes.Add(scene.path);
+        private static string[] FindEnabledEditorScenes() {
+            List<string> EditorScenes = new List<string>();
+            foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes) {
+                if (!scene.enabled) continue;
+                EditorScenes.Add(scene.path);
+            }
+            return EditorScenes.ToArray();
         }
-        return EditorScenes.ToArray();
-    }
 
-    static void PerformPCBuild() {
-        BuildPlayerOptions options = new BuildPlayerOptions();
-        options.scenes = scenes;
-        options.targetGroup = BuildTargetGroup.Standalone;
-        options.target = BuildTarget.StandaloneWindows;
-        options.locationPathName = targetPath + "/" + appname + ".exe";
-        options.options = BuildOptions.None;
-        Build(options);
-    }
+        static void PerformPCBuild() {
+            BuildPlayerOptions options = new BuildPlayerOptions();
+            options.scenes = scenes;
+            options.targetGroup = BuildTargetGroup.Standalone;
+            options.target = BuildTarget.StandaloneWindows;
+            options.locationPathName = targetPath + "/" + appname + ".exe";
+            options.options = BuildOptions.None;
+            Build(options);
+        }
 
-    static void ExportToAndroidStudio() {
-        BuildPlayerOptions options = new BuildPlayerOptions();
-        options.scenes = scenes;
-        options.targetGroup = BuildTargetGroup.Android;
-        options.target = BuildTarget.Android;
-        options.options = BuildOptions.AcceptExternalModificationsToPlayer;
-        options.locationPathName = targetPath + "/" + appname;
-        Build(options);
-    }
+        static void ExportToAndroidStudio() {
+            BuildPlayerOptions options = new BuildPlayerOptions();
+            options.scenes = scenes;
+            options.targetGroup = BuildTargetGroup.Android;
+            options.target = BuildTarget.Android;
+            options.options = BuildOptions.AcceptExternalModificationsToPlayer;
+            options.locationPathName = targetPath + "/" + appname;
+            Build(options);
+        }
 
-    static void Build(BuildPlayerOptions options) {
-        EditorUserBuildSettings.SwitchActiveBuildTarget(options.targetGroup, options.target);
-        string res = BuildPipeline.BuildPlayer(options);
-        if (res.Length > 0) {
-            throw new Exception("BuildPlayer failure: " + res);
+        static void Build(BuildPlayerOptions options) {
+            EditorUserBuildSettings.SwitchActiveBuildTarget(options.targetGroup, options.target);
+            string res = BuildPipeline.BuildPlayer(options);
+            if (res.Length > 0) {
+                throw new Exception("BuildPlayer failure: " + res);
+            }
         }
     }
 }
